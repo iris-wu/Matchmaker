9463950f49f1276d6db34a6ad173a1374009db57
LordLucifer <das3@sfu.ca>
2014-08-07 15:19:34 -0700

    Added code for adding constraint points on the texture, it also automatically adds the boundary constraints to the texture

--- gltextureselectwidget.cpp
+++ gltextureselectwidget.cpp
@@ -1,4 +1,5 @@
 #include "glTextureSelectWidget.h"
+#include "QMouseEvent"
 #include "qimage.h"
 #include "qbuffer.h"
 #include "qbytearray.h"
@@ -6,7 +7,7 @@
 glTextureSelectWidget::glTextureSelectWidget()
 {
     textureOfFace.vertexACor.x = 100;
-    textureOfFace.vertexACor.y = -100;
+    textureOfFace.vertexACor.y = 0;
     textureOfFace.vertexACor.z = 0;
     textureOfFace.vertexATex.s = 1;
     textureOfFace.vertexATex.t = 0;
@@ -17,19 +18,19 @@ glTextureSelectWidget::glTextureSelectWidget()
     textureOfFace.vertexBTex.s = 1;
     textureOfFace.vertexBTex.t = 1;
 
-    textureOfFace.vertexCCor.x = -100;
-    textureOfFace.vertexCCor.y = -100;
+    textureOfFace.vertexCCor.x = 0;
+    textureOfFace.vertexCCor.y = 0;
     textureOfFace.vertexCCor.z = 0;
     textureOfFace.vertexCTex.s = 0;
     textureOfFace.vertexCTex.t = 0;
 
-    textureOfFace.vertexDCor.x = -100;
+    textureOfFace.vertexDCor.x = 0;
     textureOfFace.vertexDCor.y = 100;
     textureOfFace.vertexDCor.z = 0;
     textureOfFace.vertexDTex.s = 0;
     textureOfFace.vertexDTex.t = 1;
 
-
+    textureLoaded = false;
 }
 
 glTextureSelectWidget::~glTextureSelectWidget()
@@ -79,22 +80,55 @@ void glTextureSelectWidget::paintGL()
     glVertexPointer(3, GL_FLOAT, 0, &textureOfFace.vertexACor.x);
     glTexCoordPointer(2, GL_FLOAT, 0,  &textureOfFace.vertexATex.s);
     glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+    if(textureLoaded)
+    {
+        //draw constrain points, don't want to use texture in this case
+        glDisable(GL_TEXTURE_2D);
+        glColor3f(0.5, 0,0.5);
+
+        //draw border constraints
+        for(int i = 0; i < 12; i++)
+        {
+            glVertexPointer(3, GL_FLOAT, 0,  &borderConstraints[i].rightBottom.x);
+            glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+        }
+
+        //draw user constraints
+        for(int i = 0; i < userConstraints.size(); i++)
+        {
+            glVertexPointer(3, GL_FLOAT, 0,  &userConstraints[i].rightBottom.x);
+            glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+        }
+
+        //all done, enable textures again
+        glEnable(GL_TEXTURE_2D);
+    }
 }
 
 void glTextureSelectWidget::resizeGL(int width, int height)
 {
+    widgetWidth = width;
+    widgetHeight = height;
     glViewport(0, 0, width, height);
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(-100, 100, -100, 100, -10, 10);
+    glOrtho(0, GL_TEXTUREWIDGET_CANVAS_WIDTH, 0, GL_TEXTUREWIDGET_CANVAS_HEIGHT, -10, 10);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 }
 
-void glTextureSelectWidget::mousePressEvent(QMouseEvent *event)
+void glTextureSelectWidget::mousePressEvent(QMouseEvent* mouseEvent)
 {
+    if(textureLoaded)
+    {
+         glTextureSelectWidget::constraintPoint point = createContraintPoint(mouseEvent->x(), mouseEvent->y());
+         userConstraints.push_back(point);
+    }
 
+    //redraw glWidget
+    updateGL();
 }
 
 void glTextureSelectWidget::loadTextureFromFile(QString& fileName)
@@ -113,4 +147,55 @@ void glTextureSelectWidget::loadTextureFromFile(QString& fileName)
 
     //should now be in openGL memory, don't need image file anymore
     delete image;
+    textureLoaded = true;
+
+    //setup border constraints
+    //Left side
+    borderConstraints[0] = createContraintPoint(0, 0);
+    borderConstraints[1] = createContraintPoint(0, widgetHeight / 3);
+    borderConstraints[2] = createContraintPoint(0, (2 * widgetHeight) / 3);
+    borderConstraints[3] = createContraintPoint(0, widgetHeight);
+    //Top side
+    borderConstraints[4] = createContraintPoint(widgetWidth / 3, 0);
+    borderConstraints[5] = createContraintPoint((2 * widgetWidth) / 3, 0);
+    borderConstraints[6] = createContraintPoint(widgetWidth, 0);
+    //Right side
+    borderConstraints[7] = createContraintPoint(widgetWidth, widgetHeight / 3);
+    borderConstraints[8] = createContraintPoint(widgetWidth, (2 * widgetHeight) / 3);
+    borderConstraints[9] = createContraintPoint(widgetWidth, widgetHeight);
+    //Bottom side
+    borderConstraints[10] = createContraintPoint(widgetWidth / 3, widgetHeight);
+    borderConstraints[11] = createContraintPoint((2 * widgetWidth) / 3, widgetHeight);
+
+    //redraw glWidget
+    updateGL();
+}
+
+glTextureSelectWidget::constraintPoint glTextureSelectWidget::createContraintPoint(int x, int y)
+{
+    int glXLocation = ((float) x / widgetWidth) * GL_TEXTUREWIDGET_CANVAS_WIDTH;
+    int glYLocation = ((float) (widgetHeight - y) / widgetHeight) * GL_TEXTUREWIDGET_CANVAS_HEIGHT;
+
+    //X and Y location is always center of the constraint point
+    constraintPoint newPoint;
+    newPoint.pixelXLocation = glXLocation;
+    newPoint.pixelYLocation = glYLocation;
+    //Left Bottom
+    newPoint.leftBottom.x = glXLocation - GL_TEXTUREWIDGET_CONSTRAINT_SIZE;
+    newPoint.leftBottom.y = glYLocation - GL_TEXTUREWIDGET_CONSTRAINT_SIZE;
+    newPoint.leftBottom.z = 0;
+    //Left Top
+    newPoint.leftTop.x = glXLocation - GL_TEXTUREWIDGET_CONSTRAINT_SIZE;
+    newPoint.leftTop.y = glYLocation + GL_TEXTUREWIDGET_CONSTRAINT_SIZE;
+    newPoint.leftTop.z = 0;
+    //Right Bottom
+    newPoint.rightBottom.x = glXLocation + GL_TEXTUREWIDGET_CONSTRAINT_SIZE;
+    newPoint.rightBottom.y = glYLocation - GL_TEXTUREWIDGET_CONSTRAINT_SIZE;
+    newPoint.rightBottom.z = 0;
+    //Right Top
+    newPoint.rightTop.x = glXLocation + GL_TEXTUREWIDGET_CONSTRAINT_SIZE;
+    newPoint.rightTop.y = glYLocation + GL_TEXTUREWIDGET_CONSTRAINT_SIZE;
+    newPoint.rightTop.z = 0;
+
+    return newPoint;
 }
--- gltextureselectwidget.h
+++ gltextureselectwidget.h
@@ -2,6 +2,11 @@
 #define GLTEXTURESELECTWIDGET_H
 
 #include <QGLWidget>
+#include <QVector>
+
+#define GL_TEXTUREWIDGET_CANVAS_WIDTH 100
+#define GL_TEXTUREWIDGET_CANVAS_HEIGHT 100
+#define GL_TEXTUREWIDGET_CONSTRAINT_SIZE 1
 
 //This widget just displays one texture mapped over the entire area of the widget
 //The user can then select certain constraint points on this texutre
@@ -19,6 +24,16 @@ class glTextureSelectWidget : public QGLWidget
     {
         float s;
         float t;
+    };
+
+    struct constraintPoint
+    {
+        int pixelXLocation;
+        int pixelYLocation;
+        vertexCor rightBottom;
+        vertexCor rightTop;
+        vertexCor leftBottom;
+        vertexCor leftTop;
     };
 
     struct rectangle //since we want to span the entire area we need a rectangle
@@ -41,13 +56,24 @@ public:
     void loadTextureFromFile(QString& fileName);
 
 protected:
+
+    //default widget gl functions
     void initializeGL(); //called once before drawing happens
     void paintGL(); //called during draw call back
     void resizeGL(int width, int height); //called when widget size changes
     void mousePressEvent(QMouseEvent *event); //when mouse is pressed inside the area
 
+    //creates a constraintPoint given the X and Y vertex coordinates relative to the widget
+    constraintPoint createContraintPoint(int x, int y);
+
+
+    int widgetWidth;
+    int widgetHeight;
+    bool textureLoaded;
     rectangle textureOfFace;
     GLuint textureId;
+    constraintPoint borderConstraints [12];
+    QVector<constraintPoint> userConstraints;
 };
 
 #endif // GLTEXTURESELECTWIDGET_H
